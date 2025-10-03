b6b73ca94b381dcef9a019ff7878b8aeaf24ebd3
Angie <vett200701@gmail.com>
2025-10-03 16:52:20 -0600

    Add files via upload

---
+++ EVA2_8_FOR/src/eva2_8_for/EVA2_8_FOR.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_8_for;
+
+/**
+ *
+ * @author Angie
+ */
+public class EVA2_8_FOR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //3 parametros
+        //variable y valor de inicio
+        //hasta donde llegas CONDICIÓN ---> TIENE QUE SER VERDAD
+        //CUANDO EL FALSO EL FOR SE DETIENE
+        //incrementa o decremento
+        //SUMAR O RESTAR
+        //OPERADORES:; AÑADIR 1 O RESTAR 1
+        //++ --
+        //i++ SIGNIFICA SUMARLE 1 AL VALOR DE LA I
+        for(int i = 1; i <=10; i++){
+            System.out.println("Hola mundo");
+        }
+        //i----- SIGNIFICA RESTARLE 1 AL VALOR LA I
+        for(int i = 10; i >=1; i--){
+            System.out.println("Hola mundo");
+        }
+    }
+}
+
+ 
+    
+
