82986e9ddcc3f69ef05487486a9920b79d668d5a
Angie <vett200701@gmail.com>
2025-10-15 22:20:11 -0600

    Add files via upload

---
+++ EVA2_16_ARREGLOS_MULTIDIM/src/eva2_16_arreglos_multidim/EVA2_16_ARREGLOS_MULTIDIM.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_16_arreglos_multidim;
+
+/**
+ *
+ * @author Angie
+ */
+public class EVA2_16_ARREGLOS_MULTIDIM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int matriz[][] = new int[3][4];
+        matriz[0][0] = 100;
+        matriz[0][1] = 200;
+        matriz[0][2] = 300;
+        matriz[0][3] = 400;
+        //ciclos anidados:dos for porque son 2 dimensiones
+        for (int i = 0; i < 3; i++){//filas
+            for(int j = 0; j < 4; j++){//columnas
+                
+                
+            }
+            
+        }
+    }
+    
+}
