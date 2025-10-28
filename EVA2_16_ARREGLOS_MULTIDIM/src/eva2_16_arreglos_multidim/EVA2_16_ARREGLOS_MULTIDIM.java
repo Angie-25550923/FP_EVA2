0c9465aae39c77b3d88b71a373b274a2bb0fd1e5
Angie <vett200701@gmail.com>
2025-10-27 21:10:14 -0600

    Add files via upload

--- EVA2_16_ARREGLOS_MULTIDIM/src/eva2_16_arreglos_multidim/EVA2_16_ARREGLOS_MULTIDIM.java
+++ EVA2_16_ARREGLOS_MULTIDIM/src/eva2_16_arreglos_multidim/EVA2_16_ARREGLOS_MULTIDIM.java
@@ -16,16 +16,32 @@ public class EVA2_16_ARREGLOS_MULTIDIM {
     public static void main(String[] args) {
         // TODO code application logic here
         int matriz[][] = new int[3][4];
-        matriz[0][0] = 100;
+        System.out.println(matriz.length);
+        System.out.println(matriz[0].length);
+        System.out.println(matriz[1].length);
+        System.out.println(matriz[2].length);
+        
+        /*matriz[0][0] = 100;
         matriz[0][1] = 200;
         matriz[0][2] = 300;
-        matriz[0][3] = 400;
+        matriz[0][3] = 400;*/
+        
+        
         //ciclos anidados:dos for porque son 2 dimensiones
-        for (int i = 0; i < 3; i++){//filas
-            for(int j = 0; j < 4; j++){//columnas
+    
+        for (int i = 0; i < matriz.length; i++){//filas
+            for(int j = 0; j < matriz[i].length; j++){//columnas
+                matriz[i][j] = (int)(Math.random () * 100);
                 
+                }
+        }
+            
+        for (int i = 0; i < matriz.length; i++){//filas
+            for(int j = 0; j < matriz[i].length; j++){//columnas
+                System.out.print("["+ matriz [i][j] + "]");
                 
             }
+            System.out.println("");
             
         }
     }
