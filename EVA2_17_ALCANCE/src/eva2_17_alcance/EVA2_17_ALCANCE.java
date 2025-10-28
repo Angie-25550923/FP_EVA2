7c93be271f85ae3c6c927aff942a3853f2fc747c
Angie <vett200701@gmail.com>
2025-10-27 21:12:14 -0600

    Add files via upload

---
+++ EVA2_17_ALCANCE/src/eva2_17_alcance/EVA2_17_ALCANCE.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_17_alcance;
+
+/**
+ *
+ * @author Angie
+ */
+public class EVA2_17_ALCANCE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i = 5;
+        {
+        int valor = 100;
+        System.out.println("");
+        }
+        System.out.println("");
+        
+        
+    }
+    }
+    
+
