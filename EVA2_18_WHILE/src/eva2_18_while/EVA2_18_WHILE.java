a9bf6c719325c1d2110f6d0ed49d986eaba31452
Angie <vett200701@gmail.com>
2025-10-28 21:53:53 -0600

    Add files via upload

---
+++ EVA2_18_WHILE/src/eva2_18_while/EVA2_18_WHILE.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_18_while;
+
+/**
+ *
+ * @author Angie
+ */
+public class EVA2_18_WHILE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("CICLO WHILE....................");
+        int i = 1;
+        while (i<10){//se repite si la condición es verdad
+            System.out.println("Angeles Salazar");
+            i++;
+        }
+        
+        //SIMULAR UN FOR CON UN WHILE
+        System.out.println("CICLO DO WHILE...............");
+        i = 1;
+        do{
+            System.out.println("Angeles Antonio");
+            i++;
+        }while(i<10);
+        
+        }
+}
+    
+
