7967bc0a9040c52cc8a8196ffb17cdb37145335c
Angie <vett200701@gmail.com>
2025-11-03 16:53:35 -0600

    Add files via upload

---
+++ EVA2_24_TRIANGULO/src/eva2_24_triangulo/EVA2_24_TRIANGULO.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_24_triangulo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Angie
+ */
+public class EVA2_24_TRIANGULO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner captu = new Scanner(System.in);
+        System.out.println("Ingrese un valor");
+        int n;
+        n = captu.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for(int j =1; j<= i; j++){
+        System.out.print("*");
+        }
+        System.out.println("");
+    }
+
+        }
+    }   
